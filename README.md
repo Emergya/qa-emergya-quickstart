89547b7bd3fda68d26b547c2f3b8e377df42a14f
aoviedo <aoviedo@emergya.com>
2020-03-09 11:18:33 +0100

    =Changes to allow default project to connect to CrossBrowserTesting

--- README.md
+++ README.md
@@ -17,13 +17,30 @@ The aim of this project is to provide a maven archetype for selenium projets usi
         </repository>
 ```
  2. Adding the following maven dependency in your ```pom.xml``` file:
+ 
 ```
-        <dependency>
-                <groupId>com.emergya</groupId>
-                <artifactId>selenium3-handler</artifactId>
-                <version>0.0.7</version>
-        </dependency>
+		<dependency>
+			<groupId>com.emergya</groupId>
+			<artifactId>selenium3Remote</artifactId>
+			<version>${selenium3-handler.version}</version>
+		</dependency>
 ```
+3. Add the following parameters in your ```pom.xml``` file, these parameters control how we connect
+to CrossBrowserTesting:
+
+```
+		<!-- Remote configuration Crossbrowsertesting -->
+		<remoteUserName>jasanchez%40emergya.com</remoteUserName>
+		<remoteUserKey>ud30d9af8f99f11e</remoteUserKey>
+		<remoteProtocol>http</remoteProtocol>
+		<remoteURL>hub.crossbrowsertesting.com:80/wd/hub</remoteURL>
+		<finalRemoteURL>${remoteProtocol}://${remoteUserName}:${remoteUserKey}@${remoteURL}</finalRemoteURL>
+		<remoteBuild>MRS_CONSOLE_1.0</remoteBuild>
+		<remoteConfiguration>Windows 10,Chrome,80x64,1600x1200</remoteConfiguration>
+		<remoteRecordVideo>true</remoteRecordVideo>
+		<remoteRecordNetwork>true</remoteRecordNetwork>
+```
+
 
 ## How to use the archetype
 Firstly, you have to clone the repository to get the project structure.
@@ -54,7 +71,7 @@ Important parameters to execute tests:
 2. selectors: In this .properties we store the way to access to every element of every page. Using Xpath, ID, etc. That method modulates and encapsulates these variables, with the advantage that entails.
 3. suites: Here we can group sets of tests to run it together.
 4. _log4j.properties_: This file sets the logging properties, in this case the output to console.
-5. _test.properties_: In this file we store some common tests properties.
+5. _test.properties_: In this file we store some common tests properties. Now we added parameters needed in our ```pom.xml``` file to connect to CrossBrowserTesting (they are placed at the end of the file)
 
 ### Items selectors
 As we explained [here] (/README.md#srcmainresources-folder), the selectors are _.properties_ files used to save the way to locate elements of the page (ID's, Xpath, etc). To make it work the _.properties_ file should have exactly the same name of the PageObject refered with the next changes: the _.properties_ file name should go in lowercase and with the suffix _Page_.
@@ -66,5 +83,7 @@ _GoogleMainPage.java_ --> _googlemainpage.properties_
 1. pageObject: Here we have every PageObjects. All PageObjects extends BasePageObject, it has important functions.
 2. testSets: Here we store the tests classes. _@before_ and _@after_ methods are defined here. All tests classes extends BasicTestSet.
 3. utils: 
- * BasicTestSet - Contains PageObjects variable references. Extends DefaultTestSet.
+ * BasicTestSet - Contains PageObjects variable references, also now handles how we set the result of our remote tests in CrossBrowserTesting. Extends DefaultTestSet.
+ * CrossBrowserInitialization - Singleton pattern class that gets the parameters needed to connect and communicate with CrossBrowserTesting.
  * Factory - Generic class to do pre-steps and post-steps method. Extends DefaultTestSet.
+ 
--- src/main/java/com/emergya/utils/BasicTestSet.java
+++ src/main/java/com/emergya/utils/BasicTestSet.java
@@ -1,19 +1,106 @@
 package com.emergya.utils;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.Capabilities;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+
 import com.emergya.pageObjects.EmergyaContactPage;
 import com.emergya.pageObjects.EmergyaMainPage;
 import com.emergya.pageObjects.EmergyaWorkPage;
 import com.emergya.pageObjects.GoogleMainPage;
 import com.emergya.selenium.testSet.DefaultTestSet;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 
 /**
  * PageObjects variable references
  * @author Jose Antonio Sanchez <jasanchez@emergya.com>
+ * @author Antonio Pérez <aoviedo@emergya.com>
+ * In this version where we connect to CrossBrowserTesting to perform our tests, this class has a new meaning
+ * Before we included here protected attributes representing the page objects we will need for our tests.
+ * Now it also handles the responsability of setting our tests to pass or fail to CrossBrowserTesting API
+ * also controlling how tests end now too
  */
 public abstract class BasicTestSet extends DefaultTestSet {
     protected GoogleMainPage googleMainPage;
     protected EmergyaMainPage emergyaMainPage;
     protected EmergyaContactPage emergyaContactPage;
     protected EmergyaWorkPage emergyaWorkPage;
+    // ************************ After Methods *************************
+    /**
+     * Now this method will set our tests to pass or fail in CrossBrowserTesting's API after our tests methods
+     * have been executed. It is really important not to overwrite the annotation @AfterMethod with another method 
+     * because in that case we could cause an error. We only could override this annotation if we also 
+     * execute this method in the method that overrides it
+     * @param result
+     */
+    @AfterMethod(alwaysRun = true)
+    public void setRemoteTestStatus(ITestResult result) {
+        log.info("---------- setRemoteTestStatus - Start ----------");
+        driver.getSessionId();
+        if ("Remote".equalsIgnoreCase(config.getBrowser())) {
+        	try {
+        		
+        		switch (result.getStatus()) {
+                case ITestResult.SUCCESS:
+                    log.info("======PASS=====");
+                    setScore(driver.getSessionId().toString(), "pass");
+                    break;
 
+                case ITestResult.FAILURE:
+                    log.info("======FAIL=====");
+                    setScore(driver.getSessionId().toString(), "fail");
+                    break;
+
+                case ITestResult.SKIP:
+                    log.info("======SKIP BLOCKED=====");
+                    setScore(driver.getSessionId().toString(), "unset");
+                    break;
+
+                default:
+                    throw new RuntimeException("Invalid status");
+                }
+        	} catch (Exception e) {
+        		System.out.println(e);
+        		System.out.println(e.getStackTrace());
+			}
+        	finally {
+        		log.info("---------- setRemoteTestStatus - End ----------");
+       
+        	}
+        }
+    }
+
+    // ************************ Private Methods *************************
+    /**
+     * This is the method that does the real task of sending a request to CrossBrowserTesting's API
+     * to set our tests pass or fail
+     * @param seleniumTestId
+     * @param score
+     * @return
+     */
+    private JsonNode setScore(String seleniumTestId, String score) {
+        // Mark a Selenium test as Pass/Fail
+        HttpResponse<JsonNode> response = null;
+
+        try {
+            response = Unirest.put(
+                    "http://crossbrowsertesting.com/api/v3/selenium/{seleniumTestId}")
+                    .basicAuth(
+                            CrossBrowserInitialization.getInstance().getRemoteUserName(),
+                            CrossBrowserInitialization.getInstance().getRemoteUserKey())
+                    .routeParam("seleniumTestId", seleniumTestId)
+                    .field("action", "set_score").field("score", score)
+                    .asJson();
+        } catch (UnirestException e) {
+            log.error(e.getMessage());
+        }
+        return response.getBody();
+    }
 }
